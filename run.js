21e27c294635b5e78750a1a6ccd36790df78752b
Brett Schules <brettschules@gmail.com>
2018-05-01 18:55:35 -0400

    generate Numbers to html

--- run.js
+++ run.js
@@ -9,30 +9,47 @@ class Operators {
 
 }
 
-class Numbers {
+class GenerateDisplay {
   constructor() {
-    this.arrayOfItems = [];
+    this.numberItems = [];
   }
 
   createNumberRows() {
     var cellDisplay = new CellDisplay();
     var newRow = false;
+    var string = '';
     for (var i = 1; i < 10; i++) {
-      // create row to only push every three number
-      this.arrayOfItems.push(`<div>${cellDisplay.render(i.toString())}</div>`)
+      // creates row to only push every three number
+      string += `<div class="col-md-3">${cellDisplay.render(i.toString())}</div>`;
+      if (i % 3 === 0) {
+        var htmlString = `<div class="row">${string}</div>`
+        this.numberItems.push(htmlString);
+        string = '';
+      }
     }
   }
 
+  addToHtml(html, element) {
+    element.innerHTML = html;
+  }
+
   render() {
-    this.arrayOfItems;
-    debugger
+    var calucatorElement = document.getElementById("calucator");
+    var joinedHtml = this.numberItems.join('');
+    this.addToHtml(joinedHtml, calucatorElement);
+    return calucatorElement
+
   }
 }
 
-var n = new Numbers();
-n.createNumberRows();
+window.onload = function(){
+  var n = new GenerateDisplay();
+  n.createNumberRows();
 
-console.log(n.render());
+  console.log(n.render());
+}
+
+
 
 class Calucator {
 
