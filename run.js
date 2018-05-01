ebc454f0ea3ddb422658e22f8af8e9a653a0f04b
Brett Schules <brettschules@gmail.com>
2018-04-30 22:11:24 -0400

    create classes and some logic

--- run.js
+++ run.js
@@ -1,1 +1,45 @@
-console.log("hi");
+class CellDisplay {
+
+  render(item) {
+    return `<div>${item}</div>`
+  }
+}
+
+class Operators {
+
+}
+
+class Numbers {
+  constructor() {
+    this.arrayOfItems = [];
+  }
+
+  createNumberRows() {
+    var cellDisplay = new CellDisplay();
+    var newRow = false;
+    for (var i = 1; i < 10; i++) {
+      // create row to only push every three number
+      this.arrayOfItems.push(`<div>${cellDisplay.render(i.toString())}</div>`)
+    }
+  }
+
+  render() {
+    this.arrayOfItems;
+    debugger
+  }
+}
+
+var n = new Numbers();
+n.createNumberRows();
+
+console.log(n.render());
+
+class Calucator {
+
+}
+
+class App {
+  constructor() {
+
+  }
+}
